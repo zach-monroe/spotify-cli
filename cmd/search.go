970eddc27a70bc79e8680e58601cb11cadea87d3
Zach <zach@Zachs-MacBook-Pro.local>
2025-11-11 12:44:11 -0500

    Adding descriptions

--- cmd/search.go
+++ cmd/search.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,13 +12,8 @@ import (
 // searchCmd represents the search command
 var searchCmd = &cobra.Command{
 	Use:   "search",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Search for the song/artist/playlist you want to cue up next",
+	Long:  `Will automatically change what is playing to what search has selected`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("search called")
 	},
--- cmd/skip.go
+++ cmd/skip.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,13 +12,8 @@ import (
 // skipCmd represents the skip command
 var skipCmd = &cobra.Command{
 	Use:   "skip",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Skip that song",
+	Long:  `Do I need to spell this one out ;)`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("skip called")
 	},
